74cfa05383cf02b2f754631b091c5aca7ab6323d
lq <lq@qq.com>
2019-09-03 21:33:06 +0800

    第一次添加

---
+++ src/main/java/com/test/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test.demo;
+
+/**
+ * Created by LQ on 2019/9/3 21:30
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("hello");
+    }
+}
